797bbd9ce689743df2ce0723d1f1f7385fcefabc
RUTAGENGWA  Justin <136063359+RUTAGENGWAj-ustin@users.noreply.github.com>
2025-08-19 18:26:35 +0200

    cors

--- server.js
+++ server.js
@@ -36,9 +36,11 @@ const __dirname = path.dirname(__filename);
 
 const app = express();
 
+
+
 // CORS configuration
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
+  res.setHeader('Access-Control-Allow-Origin', 'https://rara-fe.vercel.app');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   res.setHeader('Access-Control-Allow-Credentials', 'true');

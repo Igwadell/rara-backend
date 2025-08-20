ddcf2422132ba3c792bf582ce670025b5540b3a8
RUTAGENGWA  Justin <136063359+RUTAGENGWAj-ustin@users.noreply.github.com>
2025-08-20 15:11:28 +0200

    cors

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ const app = express();
 
 // CORS configuration
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://rara-fe.vercel.app');
+  res.setHeader('Access-Control-Allow-Origin', 'https://www.raraiwacu.com');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   res.setHeader('Access-Control-Allow-Credentials', 'true');
